846948aa793e7568c9b564b86441d1c0c517cf51
sahil <sahilwagh142@gmail.com>
2023-02-22 20:05:15 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [Hori, setHori] = useState(10)
+  const [Veri, setVeri] = useState(10)
+  const [Blur, setBlur] = useState(10)
+  const [color,setcolor]=useState('black')
+  const [bgcolor,setbgcolor]=useState('blueviolet')
+  const [checkOn,setcheckOn]=useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="controls">
+        <label >Horizontal lenght</label>
+        <input type="range" min="-200" max="200" value={Hori} onChange={(e) => setHori(e.target.value)} />
+        <label >Vertical lenght</label>
+        <input type="range" min="-200" max="200" value={Veri} onChange={(e) => setVeri(e.target.value)} />
+        <label >Blur</label>
+        <input type="range" min="0" max="200" value={Blur} onChange={(e) => setBlur(e.target.value)} />
+        <label >Color</label>
+        <input type="color" min="0" max="200" value={color} onChange={(e) => setcolor(e.target.value)} />
+        <br></br>
+        <br></br>
+        <label >CardColor</label>
+        <input type="color" min="0" max="200" value={bgcolor} onChange={(e) => setbgcolor(e.target.value)} />
+        <div class="switch">
+    <label>
+      Outline
+      <input type="checkbox" checked={checkOn} onChange={(e)=>setcheckOn(!checkOn)}/>
+      <span className="lever"></span>
+      Inline
+    </label>
+  </div>
+      </div>
+      <div className="output">
+        <div className="box" style={{boxShadow:`${checkOn?"inset":""} ${Hori}px ${Veri}px ${Blur}px ${color} ` ,backgroundColor: `${bgcolor}` }}>
+          <p>Box-Shadow: {Hori}px {Veri}px {Blur}px {color}</p>
+        </div>
+      </div>
     </div>
   );
 }
